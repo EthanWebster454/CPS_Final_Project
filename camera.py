b28ace8743800a815b01eb55f3bd258991458397
Ethan Webster <ewebster@albany.edu>
2019-12-05 17:52:15 -0500

    Added a FSM step to track temperature readings

--- camera.py
+++ camera.py
@@ -1,5 +1,6 @@
-import smtplib,ssl  
+import smtplib,ssl 
 from picamera import PiCamera  
+from PIL import Image
 from time import sleep  
 from email.mime.multipart import MIMEMultipart  
 from email.mime.base import MIMEBase  
@@ -8,17 +9,35 @@ from email.utils import formatdate
 from email import encoders  
 from sys import argv
 
-camera = PiCamera()  
+emailAddr = 'tyrantsthrall@gmail.com'
 
-camera.start_preview()  
-sleep(5)  
-camera.capture('/home/aabdelhamid/project/image.jpg')     # image path set
-sleep(5)  
-camera.stop_preview()  
+
+# snap a photo for verification if the sensor detects possible fever state
+if argv[1] == "remote":
+   
+   camera = PiCamera()  
+   camera.start_preview()  
+   sleep(1)  
+   camera.capture('/home/ethan/project/image.jpg')     # image path set
+   sleep(1) 
+   camera.stop_preview()
+
+   # rotate the image
+   origImg  = Image.open("image.jpg") 
+   rotImg = origImg.transpose(Image.ROTATE_270)
+   rotImg.save('image.jpg', 'JPEG')
+
+
 def send_an_email():  
-    toaddr = 'amr.atef91@gmail.com'      # To id 
-    me = 'amr.atef91@gmail.com'          # your id
-    subject = argv[1]              # Subject
+
+    toaddr = emailAddr      # To id 
+    me = emailAddr          # your id
+
+   # Subject
+    if argv[1] == 'room':
+       subject = 'The room temperature is in the danger zone'              
+    else:
+       subject = 'The monitoring system may have detected a fever'
   
     msg = MIMEMultipart()  
     msg['Subject'] = subject  
@@ -27,24 +46,26 @@ def send_an_email():
     msg.preamble = "test "   
     #msg.attach(MIMEText(text))  
   
-    part = MIMEBase('application', "octet-stream")  
-    part.set_payload(open("image.jpg", "rb").read())  
-    encoders.encode_base64(part)  
-    part.add_header('Content-Disposition', 'attachment; filename="image.jpg"') $
-    msg.attach(part)  
+    if argv[1] == "remote":
+      part = MIMEBase('application', "octet-stream")  
+      part.set_payload(open("image.jpg", "rb").read())  
+      encoders.encode_base64(part)  
+      part.add_header('Content-Disposition', 'attachment; filename="capture.jpg"')
+      msg.attach(part)  
   
     try:  
        s = smtplib.SMTP('smtp.gmail.com', 587)  # Protocol
        s.ehlo()  
        s.starttls()  
        s.ehlo()  
-       s.login(user = 'amr.atef91@gmail.com', password = '*******')  # User id $
+       s.login(user = emailAddr, password = 'newGMpass4$')  # User id $
        #s.send_message(msg)  
        s.sendmail(me, toaddr, msg.as_string())  
        s.quit()  
     #except:  
     #   print ("Error: unable to send email")    
     except SMTPException as error:  
-          print ("Error")                # Exception
+          print ("Error sending mail")                # Exception
   
+
 send_an_email()
--- monitor.c
+++ monitor.c
@@ -13,21 +13,27 @@
 #define REMOTE_HIGH_THRESH 37.5
 #define HUMAN_MAX_TEMP 40.0
 #define DELAY_BETW_READ 300
-#define ROOM_TEMP_DANGER "Child's room temperature in danger zone"
-#define CHILD_TEMP_DANGER "Child may have fever"
-#define PYTHON_CALL "python3 camera.py \"%s\""
+#define PYTHON_CALL "python3 camera.py %s"
+#define CAMERA_DELAY 1000
 
 
 void sendEmailAlert(bool tempType){
 
-    char buffer[100];
+    char command[100];
 
     if(tempType)
-        sprintf(buffer,PYTHON_CALL,ROOM_TEMP_DANGER);
+        sprintf(command,PYTHON_CALL,"room");
     else
-        sprintf(buffer,PYTHON_CALL,CHILD_TEMP_DANGER);
+        sprintf(command,PYTHON_CALL,"remote");
 
-    system(buffer);
+    system(command);
+}
+
+
+// experimentally determined linear calibration model for object temperature
+float calibrateValue(float measVal){
+
+    return 1.152*measVal - 3.462;
 }
 
 
@@ -41,8 +47,8 @@ int main(){
     double roomTemp;
     float remoteTemp;
     size_t i;
-    bool direction = 1;
-    void (*rotate)(int,int);
+    bool roomTempError=0, remTempError=0, direction = 1;
+    void (*rotate)(int,int); // function pointer for rotation
 
     initLCD();
 
@@ -67,22 +73,34 @@ int main(){
             printRoomTemp(roomTemp);
 
         // if roomTemp > highThresh or < lowThresh, perform alarm
-        if(roomTemp != ERROR && (roomTemp > ROOM_HIGH_THRESH || roomTemp < ROOM_LOW_THRESH)){
+        // only perform the alarm once. The system is reset once the temperature is back inside the safe zone
+        if(!roomTempError && roomTemp != ERROR && (roomTemp > ROOM_HIGH_THRESH || roomTemp < ROOM_LOW_THRESH)){
+            roomTempError=true;
             makeSound();
             sendEmailAlert(true);
         }
+        // this resets the tracker variable if the situation is resolved
+        else if(roomTemp != ERROR && roomTemp < ROOM_HIGH_THRESH && roomTemp > ROOM_LOW_THRESH)
+            roomTempError=false;
 
         for(i=0;i<NUM_STEPS;i++){
 
             remoteTemp = getRemoteTemp();
-
-            printRemoteTemp(remoteTemp);
+            if(remoteTemp>0){
+                remoteTemp = calibrateValue(remoteTemp); // calibrate temperature read using D6T
+                printRemoteTemp(remoteTemp);
+            }
 
             // temperature is in the danger zone, perform alarm
-            if(remoteTemp > REMOTE_HIGH_THRESH && remoteTemp < HUMAN_MAX_TEMP) {
+            if(!remTempError && remoteTemp != ERROR && (remoteTemp > REMOTE_HIGH_THRESH && remoteTemp < HUMAN_MAX_TEMP)) {
+                remTempError = true;
                 makeSound();
                 sendEmailAlert(false);
+                delay(CAMERA_DELAY);
             }
+            // this resets the tracker variable if another person is present
+            else if(remoteTemp != ERROR && remoteTemp < REMOTE_HIGH_THRESH)
+                remTempError=false;
 
             // rotate the motor by the correct increment
             rotate(MOTOR_DEL, ANGLE_INCR);
